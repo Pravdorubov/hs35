12e6d97364c39c99335897af8b60075a0ecd22bf
Pravdorubov <pravdorubov.nn@gmail.com>
2018-05-29 00:05:15 +0300

    начало питона

--- site_hs35/models.py
+++ site_hs35/models.py
@@ -2,32 +2,44 @@ from django.db import models
 
 class STATUS:
     PUPIL = 'PUPIL'
-    TEACHER = 'PUPIL'
+    TEACHER = 'TEACHER'
     PARENT = 'PARENT'
 
 
+class DISCIPLINE:
+    MATH = 'Mathematic'  
+    INFO = 'Informatic'   
+
+
 STATUS_CHOICES = (
-    (STATUS.PUPIL, 'Ученик', 'Ankete'),
-    (STATUS.TEACHER, 'Учитель', 'Ankete'),
-    (STATUS.PARENT, 'Родитель', 'Ankete'),
+    (STATUS.PUPIL, 'Ученик'),
+    (STATUS.TEACHER, 'Учитель'),
+    (STATUS.PARENT, 'Родитель'),
+)
+
+DISCIPLINE_CHOICES = (
+    (DISCIPLINE.MATH, 'Математика'),
+    (DISCIPLINE.INFO, 'Информатика'),
 )
 
 class Ankete(models.Model):
     '''
     Анкета
     '''
-    status = models.CharField('Статус', max_length=6, choices=SOURCE_CHOICES, default=STATUS.PUPIL)
-    name = models.CharField('Имя')
+    status = models.CharField('Статус', max_length=6, choices=STATUS_CHOICES, default=STATUS.PUPIL)
+    name = models.CharField('Имя', max_length=256)
+    wishes = models.CharField('Пожелания', max_length=1000)
+    discipline = models.CharField('Дисциплина', max_length=10, choices=DISCIPLINE_CHOICES, default=DISCIPLINE.PUPIL)
 
     def __str__(self):
-        if self.name_short:
-            return self.name_short
-        return self.name_full       
+        if self.name:
+            return self.name
+        return self.name       
 
     class Meta:
         verbose_name = 'Исполнитель'
         verbose_name_plural = 'Исполнители'
-        ordering = ['name_full']
+        ordering = ['name']
 
     @classmethod
     def get_no_executor(cls):
