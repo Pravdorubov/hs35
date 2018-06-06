efde76fdcd17142a0f0a1c8cf045bbac4fca0a06
Pravdorubov <pravdorubov.nn@gmail.com>
2018-06-07 00:03:16 +0300

    cssing form

--- site_hs35/forms.py
+++ site_hs35/forms.py
@@ -7,4 +7,9 @@ class AnketeForm(forms.ModelForm):
     class Meta:
         model = Ankete
         fields = '__all__'
-
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'status': forms.TextInput(attrs={'class': 'select-dropdown'}),
+            'discipline': forms.TextInput(attrs={'class': 'select-dropdown'}),
+            'wishes': forms.TextInput(attrs={'class': 'form-control'}),
+        }
